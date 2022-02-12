c3e2a39c6c49495b5389ecf607a14de8c9b2f080
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-02-12 00:47:36 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,14 +7,14 @@
           <IconNuxt alt="Nuxt" />
         </div>
         <h1 class="mt-4 text-4xl font-extrabold leading-10 text-white sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">
-          Check out Nuxt Modules abc
+          Check out Nuxt Modules 
         </h1>
         <p class="max-w-xl mx-auto mt-5 text-lg leading-7 sm:text-xl text-rainy-grey">
           Discover our list of modules to supercharge your <a href="https://nuxtjs.org" rel="noopener" target="_blank" class="border-b border-stone-green hover:text-green-500 hover:border-green-600">Nuxt project</a>. Created by the Nuxt team and community.
         </p>
         <div class="max-w-xl mx-auto mt-2 text-center">
           <a href="https://github.com/nuxt/modules" rel="noopener" target="_blank" class="items-center space-x-1 leading-4 border-b text-md text-grey-light border-stone-green hover:text-green-500 hover:border-green-600">
-            Contribute on abc GitHub
+            Contribute on GitHub
           </a>
         </div>
       </div>
